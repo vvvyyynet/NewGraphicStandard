8a789a4abed326f0bab618249c585c895d202b19
vvvyyynet <levyn.buerki@unibe.ch>
2025-06-15 17:45:11 +0200

    feat: add Menu component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-
+	import Menu from './Menu.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid md:grid-cols-2">
+	<Menu />
+	{@render children()}
+</div>
---
+++ src/routes/Menu.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<div class="absolute bg-primary-500 h-full w-full">
+	<div class="grid grid-cols-[auto_80px]">
+		<h1>New Graphic Standard</h1>
+		<div class="flex flex-col">
+			<button>X</button>
+			<button>d</button>
+		</div>
+	</div>
+	<nav>
+		<ol>
+			<li><a href="#">Info</a></li>
+			<li><a href="#">Angebot</a></li>
+			<li><a href="#">Toolbox</a></li>
+			<li><a href="#">Friends</a></li>
+		</ol>
+	</nav>
+
+	<nav>
+		<ol>
+			<li><a href="#">Kontakt</a></li>
+			<li><a href="#">Unterstütze uns!</a></li>
+			<li><a href="#">Impressum</a></li>
+		</ol>
+	</nav>
+
+	<button>CO2</button>
+</div>

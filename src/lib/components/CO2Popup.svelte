a935adb5a4502ed9718d06ca8575f468b5c760e1
vvvyyynet <levyn.buerki@unibe.ch>
2025-06-16 11:21:19 +0200

    feat: add CO2Popup

---
+++ src/lib/components/CO2Popup.svelte
@@ -0,0 +1,19 @@
+<script>
+	let amount = $state(99);
+	const close = () => {
+		console.log('closing');
+	};
+</script>
+
+<div class="bg-white rounded-lg p-10 flex gap-5 max-w-200 absolute max-h-80 bottom-20 right-20 border">
+	<p>🌍</p>
+	<p>
+		Diese Webseite hat einen CO<sub>2</sub>-Abdruck von {amount} CO<sub>2</sub>-eq pro
+		Seitenansicht. Erfahre <a href="co2">hier</a> mehr dazu.
+	</p>
+	<button
+		onclick={() => {
+			close();
+		}}>x</button
+	>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Co2Popup from "$lib/components/CO2Popup.svelte";
+</script>
+
+<Co2Popup />

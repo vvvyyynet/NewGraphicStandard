ea4032bee2a7bb095944dfaab75222a70d4139c8
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-24 16:40:43 +0200

    feat: add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Multipage from '$lib/components/Multipage.svelte';
+	import Error from './Error.md';
+
+	let route = $derived(page.url.pathname.split('/').pop());
+</script>
+
+<Multipage title="404" {route}>
+	<Error />
+</Multipage>

4fac7e48282c8b98e1497bf5ed4822a0eeafc8ff
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-27 04:45:38 +0200

    feat: add filter for tiles

--- src/routes/toolbox/tools/+page.svelte
+++ src/routes/toolbox/tools/+page.svelte
@@ -3,6 +3,7 @@
 	import Tile from '$lib/components/Tile.svelte';
 	import { onMount } from 'svelte';
 	import Columns from '$lib/components/Columns.svelte';
+	import { Switch } from '@skeletonlabs/skeleton-svelte';
 
 	let { data }: PageProps = $props();
 
@@ -23,13 +24,25 @@
 				sum2 += tool.size;
 			}
 		}
-		console.log(`${sum1} (${toolsSet1.length}), ${sum2} (${toolsSet2.length})`);
 		return { toolsSet1, toolsSet2, sum1, sum2 };
 	}
 
-	// Shuffle tools and split by size (the size is set manually in 'tools-list.json')
-	const tools = [...data.tools].sort(() => Math.random() - 0.5);
-	let { toolsSet1, toolsSet2 } = splitToolsBySize(tools);
+	// Shuffle tools
+	// Filter tools
+	let tools = [...data.tools].sort(() => Math.random() - 0.5);
+	let allowedTags = $state(['very serious']);
+	let useAllTags = $state(true);
+	// split by size (the size is set manually in 'tools-list.json')
+	let tools_filtered = $derived(
+		tools.filter((tool) => {
+			if (useAllTags) {
+				return allowedTags.every((tag) => tool.tags.includes(tag));
+			} else {
+				return allowedTags.some((tag) => tool.tags.includes(tag));
+			}
+		})
+	);
+	let { toolsSet1, toolsSet2 } = $derived(splitToolsBySize(tools_filtered));
 
 	// Load all Abstracts
 	let Abstracts: Record<string, any> = $state({});
@@ -39,7 +52,49 @@
 			Abstracts[tool.slug] = (await import(`./${tool.folder}/Abstract.md`)).default;
 		}
 	});
+
+	// Functions for Filters
+	const toggleTag = (tag) => {
+		allowedTags = allowedTags.includes(tag)
+			? allowedTags.filter((t) => t !== tag)
+			: [...allowedTags, tag];
+	};
 </script>
+
+<!-- Filters -->
+<div class="dark:bg-primary-500 sticky top-0 mb-10 bg-white pt-1 pb-4">
+	<h2 class="mb-2 text-sm">Nach Kategorie filtern</h2>
+	<div class="flex flex-wrap gap-2">
+		<div class="flex flex-wrap gap-2">
+			{#each Array.from(new Set(data.tools.flatMap((tool) => tool.tags))) as tag (tag)}
+				<button
+					onclick={() => toggleTag(tag)}
+					class={[
+						'border-primary-500 text-primary-500 hover:bg-secondary-500 hover:text-primary-500 rounded-full border-2 px-3 py-1 text-xs uppercase dark:border-white ',
+						allowedTags.includes(tag) &&
+							'bg-secondary-500 text-primary-500 hover:text-primary-500 hover:bg-transparent hover:dark:text-white',
+						!allowedTags.includes(tag) && 'dark:text-white'
+					]}
+				>
+					{tag}
+				</button>
+			{/each}
+		</div>
+		<Switch
+			checked={useAllTags}
+			classes="ml-5"
+			controlClasses="border border-primary-500 dark:border-white"
+			controlActive="bg-white dark:bg-primary-500"
+			controlInactive="bg-white dark:bg-primary-500"
+			thumbClasses="border border:primary-500 bg-primary-500"
+			thumbInactive="bg-primary-500 dark:bg-white"
+			thumbActive="bg-primary-500 dark:bg-white"
+			onCheckedChange={(e) => (useAllTags = e.checked)}
+		>
+			{useAllTags ? 'Strikt' : 'Locker'}
+		</Switch>
+	</div>
+</div>
 
 <Columns ncols={[2, 2, 1]} gap={8} classes="mx-auto px-[5vw]">
 	<!-- <div class="flex max-h-[200vh] flex-col gap-4 lg:flex-wrap"> -->

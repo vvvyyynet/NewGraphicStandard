eb742cba05c3458249d238870741a347edb40f70
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-27 03:18:27 +0200

    feat: add tags to Content of tool

--- src/routes/toolbox/tools/font-diversity/+page.svelte
+++ src/routes/toolbox/tools/font-diversity/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import Page from './Page.md';
+	let { data }: PageProps = $props();
+	import { page } from '$app/state';
+	let route = $derived(page.url.pathname.split('/').pop());
+	let tags = $derived(data.tools.find((tool) => tool.folder == route)?.tags);
 </script>
 
 <div class={['prose-base md:text-md h-full w-full min-w-[100%] pb-20 lg:text-lg']}>
+	<ul class="p-0">
+		{#each tags as tag}
+			<li class="dark:text-secondary-500 mr-2 inline p-0 font-bold">#{tag}</li>
+		{/each}
+	</ul>
 	<Page />
 </div>

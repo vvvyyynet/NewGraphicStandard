472a2f2cea04e161ac99729ea0f2d8e4de9ddb83
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-27 03:16:31 +0200

    chore: simplify Tile

--- src/lib/components/Tile.svelte
+++ src/lib/components/Tile.svelte
@@ -3,24 +3,19 @@
 	let { hasContent = false, classes = '', href = '', children } = $props();
 </script>
 
-{#if hasContent}
-	<button
-		class={[
-			'bg-primary-500 hover:bg-primary-100 dark:text-primary-500 hover:text-primary-500 border-primary-500 text-left text-white dark:border-white dark:bg-white',
-			classes
-		]}
-		onclick={() => {
-			goto(href);
-		}}
-	>
-		<div class={['']}>
-			{@render children()}
-		</div>
-	</button>
-{:else}
-	<div class={['border-primary-500 text-primary-500 dark:border-white dark:text-white', classes]}>
-		<div class={['']}>
-			{@render children()}
-		</div>
+<div
+	class={[
+		'border-primary-500 text-left',
+		hasContent &&
+			'bg-primary-500 hover:bg-primary-100 dark:text-primary-500 hover:text-primary-500 text-white dark:border-white dark:bg-white',
+		!hasContent && 'text-primary-500 dark:border-white dark:text-white',
+		classes
+	]}
+>
+	{#if hasContent}
+		<a {href} class="absolute h-full w-full hover:cursor-pointer"> </a>
+	{/if}
+	<div class={['']}>
+		{@render children()}
 	</div>
-{/if}
+</div>

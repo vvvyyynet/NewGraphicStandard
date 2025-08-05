4e17b02b6e59a71add0e251e35b6f5f1c6a448b5
vvvyyynet <levyn.buerki@unibe.ch>
2025-08-05 07:04:20 +0200

    chore: use typescript PageLoad

--- src/routes/toolbox/tools/+layout.ts
+++ src/routes/toolbox/tools/+layout.ts
@@ -1,7 +1,8 @@
 export const prerender = true;
 
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
     const res = await fetch('/data/tools-list.json');
     const tools = await res.json(); 
     return {tools};

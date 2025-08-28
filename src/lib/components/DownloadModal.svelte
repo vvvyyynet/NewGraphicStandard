cd61893831de268df177f4994238369f7f494c55
Katharina Scheller <125909828+schellerk@users.noreply.github.com>
2025-08-28 21:36:18 +0200

    change text billing options

--- src/lib/components/DownloadModal.svelte
+++ src/lib/components/DownloadModal.svelte
@@ -48,7 +48,7 @@
 
 			<div class="flex flex-wrap items-center justify-center gap-5">
 				<img src="/img/qr-IBAN.png" alt="QR Code für E-Banking" class="h-30 w-30" />
-				<p class="text-lg">IBAN CH85 0076 9441 4721 0200 1</p>
+				<p class="text-lg">Bezahlung per Banküberweisung: IBAN CH85 0076 9441 4721 0200 1</p>
 				<p class="text-lg">Oder: Bezahlung per TWINT unter diesem ↗ <a href="https://go.twint.ch/1/e/tw?tw=acq.sGZu1zkzT0aZyvZ233nYMU8Xp1puBm-itMl-a5b6zPbGD4K0aIFr1dHETMT5592P." style="text-decoration: underline;" target="_blank">Link</a></p>
 			</div>
 			<!-- <p class="">Download-Optionen</p> -->

4f1ae9b0731b9f77aac44fa0c6f6b6ec1b96454f
Katharina Scheller <125909828+schellerk@users.noreply.github.com>
2026-05-15 16:27:22 +0200

    change first page to Talks

--- src/routes/angebot/+page.svelte
+++ src/routes/angebot/+page.svelte
@@ -4,6 +4,6 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		goto(`${base}/angebot/beratung`, { replaceState: true });
+		goto(`${base}/angebot/talks`, { replaceState: true });
 	});
 </script>
